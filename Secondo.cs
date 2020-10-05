4ec4e3c2e0f94be84ff0f2cc3168d143770d7723
Flavio Gavarini <Flaviogavarini@gmail.com>
2020-10-05 10:21:39 +0200

    Secondo commit

---
+++ Secondo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GitTest
+{
+    class Muori
+    {
+        public string nome { get; set; }
+    };
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi chaimo Flavio");
+        }
+    }
+}
