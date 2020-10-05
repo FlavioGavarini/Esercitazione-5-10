9daa71839de6e8a594f97c5ab3ad6f4ebd948dfb
Flavio Gavarini <Flaviogavarini@gmail.com>
2020-10-05 10:24:33 +0200

    Terzo commit

---
+++ Terzo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GitTest
+{
+    class Muori
+    {
+        public string nome { get; set; }
+    };
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Classe 5IA");
+        }
+    }
+}
